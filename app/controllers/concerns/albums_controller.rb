9faf2ff443ca52cbdd42da39811474e9f5421469
ROBERT_STOELHORST <robbiestoelhorst@hotmail.com>
2020-11-13 10:28:26 +1100

    bugs

--- app/controllers/concerns/albums_controller.rb
+++ app/controllers/concerns/albums_controller.rb
@@ -5,6 +5,10 @@ class AlbumsController < ApplicationController
     @albums = @current_user.albums
   end
 
+  def show
+    @album = Album.find(params[:id])
+  end
+
   def new
     @album =  Album.new
   end
