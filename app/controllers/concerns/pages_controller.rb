c4b5ae2867aec1d33cffabfee452e6143107d026
Guillaume Canneton <g.canneton@gmail.com>
2020-11-13 10:23:48 +1100

    models cleaning

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -1,10 +1,5 @@
 class PagesController < ApplicationController
   def home
-    if @current_user.present?
-      @pictures = @current_user.pictures
-    else
-      @pictures = Picture.all
-    end
-      @picture = Picture.all.sample
+    
   end
 end
--- app/models/picture.rb
+++
@@ -1,9 +0,0 @@
-class Picture < ApplicationRecord
-
-  belongs_to :user, :optional => true
-  belongs_to :album, :optional => true
-  has_many :likes
-  def liked?(user)
-    !!self.likes.find{|like| like.user_id == user.id}
-  end
-end
